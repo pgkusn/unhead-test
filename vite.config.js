499e2339f61ead9919be9841bd34b45aa10489a7
Kenge <pgkusn@gmail.com>
2025-07-02 14:26:00 +0800

    fix: update base path for GitHub Actions deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/vue-project/' : '/',
+  base: process.env.GITHUB_ACTIONS ? '/unhead-test/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
