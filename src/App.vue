77e262a71f3942b7bdc99c1b4717f941acf7841f
Kenge <pgkusn@gmail.com>
2025-07-03 00:07:11 +0800

    Revert "feat: 更新 SEO 元數據以動態顯示文章標題和摘要"
    This reverts commit 0e9038c54a12283b024bb8a99c17686528516310.

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,22 @@
 <script setup>
-import { ref, computed, watchEffect } from 'vue'
-import { useSeoMeta, useHead } from '@unhead/vue'
-
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 
-// useHead({
-//   titleTemplate: title => {
-//     // 當子元件傳入 title 時，使用該 title；否則，只顯示網站名稱
-//     return title ? `${title} - 我的神奇網站` : '我的神奇網站'
-//   },
-// })
-
-// const ogTitle = ref('When Great Minds Don’t Think Alike')
-// useSeoMeta({
-//   ogUrl: 'https://pgkusn.github.io/unhead-test/',
-//   ogType: 'article',
-//   ogTitle,
-//   ogDescription: 'How much does culture influence creative thinking?',
-//   ogImage:
-//     'http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg',
-// })
-
-const post = ref(null)
-
-// 透過 setTimeout 模擬非同步取得文章資料
-setTimeout(() => {
-  post.value = {
-    title: '模擬文章標題',
-    summary: '這是一段模擬的文章摘要內容。',
-    coverImage: 'https://via.placeholder.com/600x300.png?text=Cover+Image',
-  }
-}, 3000)
+import { useSeoMeta } from '@unhead/vue'
 
 useSeoMeta({
-  title: () => post.value?.title || '讀取中...',
-  description: () => post.value?.summary,
-  ogTitle: () => post.value?.title,
-  ogDescription: () => post.value?.summary,
-  ogImage: () => post.value?.coverImage,
+  ogUrl: 'https://pgkusn.github.io/unhead-test/',
+  ogType: 'article',
+  ogTitle: 'When Great Minds Don’t Think Alike',
+  ogDescription: 'How much does culture influence creative thinking?',
+  ogImage:
+    'http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg',
 })
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-    <!-- <input type="text" v-model="ogTitle" placeholder="Enter a title for SEO" /> -->
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
